0df65c2a855c539311b65fcd0fd0d692d98af010
piotrprus <piotr.prus@schibsted.com>
2020-05-27 21:00:25 +0200

    change start screen assets added

---
+++ lib/Styles.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const textPrimaryGrey = Color(0xff707070);
+const backgroundGreen = Color(0xffC7FFEA);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ekoasia/ChooseCityScreen.dart';
+import 'package:ekoasia/Styles.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Eko Asia',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,18 +23,47 @@ class _WelcomePageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            child: Text('Start'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ChooseCityScreen()),
-              );
-            },
+        body: Container(
+          decoration: BoxDecoration(
+            color: backgroundGreen
+          ),
+          child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  child: Image(
+                    image: AssetImage('assets/images/asiaImage.png'),
+                  ),
+                ),
+                Text('Eko Asia',
+                    style: TextStyle(
+                        fontSize: 50
+                    )),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 60),
+                  child: Text('Cześć, jestem tu, by pomóc ci w segregacji odpadów.',
+                    style: TextStyle(
+                        fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: textPrimaryGrey
+                    ),),
+                ),
+                RaisedButton(
+                  color: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text('Start', style: TextStyle(
+                    fontSize: 22
+                  ),),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ChooseCityScreen()),
+                    );
+                  },
+                )
+              ]
           ),
         ));
   }

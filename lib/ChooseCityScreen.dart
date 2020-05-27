d52fb17d7a5295b37c1d2d9f5df7f413faabad5a
piotrprus <piotr.prus@schibsted.com>
2020-05-27 22:05:28 +0200

    choose your city screen changed style

--- lib/ChooseCityScreen.dart
+++ lib/ChooseCityScreen.dart
@@ -1,3 +1,4 @@
+import 'package:ekoasia/Styles.dart';
 import 'package:ekoasia/recordSearchScreen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,38 +8,42 @@ class ChooseCityScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Choose your location"),
+      body: Container(
+        color: backgroundGrey,
+        child: Center(child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text('Wybierz w jakim mieście jesteś',
+              style: TextStyle(
+                fontSize: 22,
+                color: textPrimaryGrey,
+                fontWeight: FontWeight.bold
+              ),
+              textAlign: TextAlign.center),
+            ),
+            IconButton(
+              iconSize: 250,
+              icon: Image.asset('assets/images/gdansk_button.png'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => RecordSearchScreen(city: ChosenCity('gdansk', 'gd'),)));
+                print("gdansk pressed");
+              },
+            ),
+            IconButton(
+              iconSize: 250,
+              icon: Image.asset('assets/images/krakow_button.png'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => RecordSearchScreen(city: ChosenCity('krakow', 'kr'),)));
+                print("krakow pressed");
+              },
+            )
+          ],
+        )),
       ),
-      body: Center(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('Wybierz w jakim mieście jesteś',
-          style: TextStyle(
-            fontSize: 50,
-            color: Colors.grey
-          ),
-          textAlign: TextAlign.center),
-          IconButton(
-            iconSize: 225,
-            icon: Image.asset('assets/images/gdansk_button.png'),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => RecordSearchScreen(city: ChosenCity('gdansk', 'gd'),)));
-              print("gdansk pressed");
-            },
-          ),
-          IconButton(
-            iconSize: 225,
-            icon: Image.asset('assets/images/krakow_button.png'),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(
-                  builder: (context) => RecordSearchScreen(city: ChosenCity('krakow', 'kr'),)));
-              print("krakow pressed");
-            },
-          )
-        ],
-      )),
     );
   }
 

aff542b06347c167b0840c47038a2f05c32da9e5
piotrprus <piotr.prus@schibsted.com>
2020-05-27 21:59:35 +0200

    start screen design finished

--- lib/Styles.dart
+++ lib/Styles.dart
@@ -2,3 +2,4 @@ import 'dart:ui';
 
 const textPrimaryGrey = Color(0xff707070);
 const backgroundGreen = Color(0xffC7FFEA);
+const primaryGreen = Color(0xff33D093);
--- lib/main.dart
+++ lib/main.dart
@@ -24,47 +24,56 @@ class _WelcomePageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          decoration: BoxDecoration(
-            color: backgroundGreen
+      decoration: BoxDecoration(color: backgroundGreen),
+      child: Column(children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 100, bottom: 40),
+          child: Image(
+            image: AssetImage('assets/images/asiaImage.png'),
           ),
-          child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  child: Image(
-                    image: AssetImage('assets/images/asiaImage.png'),
-                  ),
-                ),
-                Text('Eko Asia',
-                    style: TextStyle(
-                        fontSize: 50
-                    )),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 60),
-                  child: Text('Cześć, jestem tu, by pomóc ci w segregacji odpadów.',
-                    style: TextStyle(
-                        fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                      color: textPrimaryGrey
-                    ),),
-                ),
-                RaisedButton(
-                  color: Colors.green,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Text('Start', style: TextStyle(
-                    fontSize: 22
-                  ),),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ChooseCityScreen()),
-                    );
-                  },
-                )
-              ]
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text('Eko Asia',
+              style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: textPrimaryGrey)),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 60),
+          child: Text(
+            'Cześć, jestem tu, by pomóc ci w segregacji odpadów.',
+            style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: textPrimaryGrey),
           ),
-        ));
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 100),
+          child: RaisedButton(
+            color: primaryGreen,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 24, top: 12, right: 24, bottom: 12),
+              child: Text(
+                'START',
+                style: TextStyle(fontSize: 22, color: Colors.white),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ChooseCityScreen()),
+              );
+            },
+          ),
+        )
+      ]),
+    ));
   }
 }

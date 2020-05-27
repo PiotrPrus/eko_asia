80fc99fc34b0b1f53ab5f861ac4654efc094ed77
Kamajabu <kamil.buczel@kamajabu.pl>
2020-05-27 16:04:05 +0200

    Updated product model

--- lib/Product.dart
+++ lib/Product.dart
@@ -1,9 +1,18 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Product {
+  String documentID;
+
   String name;
-  String questions;
+  String binType;
 
   String binName() {
     return "Happy Bin";
   }
+
+  Product.fromSnapshot(DocumentSnapshot snapshot)
+      : documentID = snapshot.documentID,
+        name = snapshot['name'],
+        binType = snapshot['binType'];
 }
